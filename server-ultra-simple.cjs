de29d93459ec8562a40e246ca21027ee33b5f4d6
Regis <regis649@googlemail.com>
2026-07-02 21:38:04 +0100

    force: Trigger Render redeploy with .cjs file

--- server-ultra-simple.cjs
+++ server-ultra-simple.cjs
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-console.log('Starting ultra-simple server...');
+console.log('✅ Starting ultra-simple CommonJS server v2...');
 
 const server = http.createServer((req, res) => {
   console.log(`Request: ${req.method} ${req.url}`);
